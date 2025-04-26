035962b41218239c0d258155d001d0337a77855b
Jane <janefrancesokure47@gmail.com>
2025-04-26 03:59:44 +0100

    Initial Commit

---
+++ ShopSavvyy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopSavvyy
+//
+//  Created by Jane on 26/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShopSavvyy/ShopSavvyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSavvyyApp.swift
+//  ShopSavvyy
+//
+//  Created by Jane on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopSavvyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
